4e11e397c27c31bf898f64392e71233de4c16a4c
LukeZem <102622914+LukeZem@users.noreply.github.com>
2022-05-01 17:10:33 -0700

    messing around with js

---
+++ test/src/index.js
@@ -0,0 +1,8 @@
+
+console.log('index.js is linked')
+
+const button = document.querySelector('.event-button');
+button.addEventListener('click', function () {
+    alert(message);
+});
+let message = "whats up?";
